ea4a249a5c5e2b01603ee4e809c13973121ac67f
tubtim <tubtim_kuph@hotmail.com>
2021-04-27 12:24:15 +0700

    add constance

---
+++ src/constants/routName.js
@@ -0,0 +1,7 @@
+export const CONTACT_LIST = 'Contacts';
+export const CONTACT_DETAIL = 'Contact Detail';
+export const CREATE_CONTACT = 'Create Contact';
+export const SETTING = 'Settiing';
+export const LOGIN = 'Login';
+export const REGISTER = 'Register';
+export const HOME_NAVIGATOR = 'Home Navigator';
